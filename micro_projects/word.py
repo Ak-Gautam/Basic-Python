624e1494a68882505699e3efb605130900769c22
Gautam <61274514+Gautam-flash@users.noreply.github.com>
2020-06-21 13:07:42 +0530

    Create word.py
    A python program to find the no. of words that can be formed using a given set of characters from a given dictionary.

---
+++ micro_projects/word.py
@@ -0,0 +1,23 @@
+#This program takes a list of words and a list of letters as input and find the words that can be formed using the letters .
+#A character can't appear twice consecutively but can appear twice in the same word. i.e. Ball: wrong  Bomb:correct
+
+def charCount(i):  #this function counts the number of times the word has appeared and creates a dictionary based on it.
+    dict = {} 
+    for j in i: 
+        dict[i] = dict.get(j, 0) + 1
+    return dict
+
+def possible_words(words, charSet):  #This function checks is the word is possible
+    for i in words: 
+        flag = 1
+        chars = charCount(i) 
+        for key in chars: 
+            if key not in charSet: 
+                flag = 0
+        else: 
+            if charSet.count(key) != chars[key]: 
+                flag = 0
+        if flag == 1: 
+            print(i) 
+
+ 
