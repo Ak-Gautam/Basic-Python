5a3174b57ae3dc16e4ea8289c5afda4adb55ac30
Gautam <61274514+Gautam-flash@users.noreply.github.com>
2020-06-24 12:20:11 +0530

    Added second method
    Added the way to move zeroes without using extra variable

--- micro_projects/move_zeroes.py
+++ micro_projects/move_zeroes.py
@@ -1,6 +1,4 @@
 # This one moves zeroes to the end of the array while maintaining the correct order of other elements
-a = [0, 1, 12, 0, 17]
-
 def move_ze(a):
     for i in range(len(a)):
         if a[i] == 0:
@@ -9,5 +7,21 @@ def move_ze(a):
                 a[j] = a[j+1]
             a[len(a)-1] = temp
     return a
-move_ze(a)
+
+#Moving zeroes without using extra variable. 
+def move_zer(a):
+    indx = 0
+    for i in range(len(a)):
+        if a[i] != 0:
+            a[indx] = a[i]
+            indx += 1
+    for j in range(indx , len(a)):
+        a[j] = 0
+    return a
+#Taking user input for the array
+a = []
+for _ in range(int(input("Enter the length of the array\n"))):
+    a.append(int(input("Enter the array elements\n")))
+move_zer(a)
+move_ze(a)            
             
